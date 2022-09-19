bc299a09c6d91fd88d3e5708601992438c398004
Varun Ramakrishnan <oceans.varun@gmail.com>
2022-09-19 15:22:16 -0400

    Check if ray hits a sphere
    - Set color as red if ray hits sphere

--- RayTracing/main.cpp
+++ RayTracing/main.cpp
@@ -5,18 +5,49 @@
 #include "ray.h"
 
 /// <summary>
+/// Returns true if the given ray hits a sphere of the given radius and center
+/// </summary>
+/// <param name="center">Center of the sphere</param>
+/// <param name="radius">Radius of the sphere</param>
+/// <param name="r">Ray</param>
+/// <returns></returns>
+bool hit_sphere(const vec3& center, float radius, const ray& r)
+{
+	vec3 oc = r.origin() - center; // Make ray from origin to center of sphere
+
+	// Get roots of equation
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0f * dot(oc, r.direction());
+	float c = dot(oc, oc) - radius * radius;
+
+	float discriminant = b * b - 4 * a * c;
+
+	return (discriminant > 0);
+}
+
+/// <summary>
 /// Linearly blend white and blue depending on the y coordinate
 /// </summary>
 /// <param name="r">Ray</param>
 /// <returns></returns>
-vec3 lerp(const ray& r)
+vec3 set_color(const ray& r)
 {
+	// Check if rayu hits sphere
+	if (hit_sphere(vec3(0.0, 0.0, -1.0), 0.5f, r))
+	{
+		return vec3(1.0, 0.0, 0.0); // Set color as red if hit
+	}
+
 	vec3 unit_dir = unit_vector(r.direction()); // Make unit vector
 	float t = 0.5f * (unit_dir.y() + 1.0f); // Change range from [-1, 1] to [0, 1]
 
 	return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
 }
 
+/// <summary>
+/// Write a ray tracer into a .ppm file
+/// </summary>
+/// <returns></returns>
 int main()
 {
 	std::ofstream file("image.ppm");
@@ -41,7 +72,7 @@ int main()
 
 			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
 			
-			vec3 col = lerp(r);
+			vec3 col = set_color(r);
 
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
