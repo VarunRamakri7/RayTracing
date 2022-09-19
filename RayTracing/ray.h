098577aa2899592144a73a3076a8d3f6b96f0d92
Varun Ramakrishnan <oceans.varun@gmail.com>
2022-09-19 16:00:54 -0400

    Updated header files
    - Added color and rtweekend

--- RayTracing/ray.h
+++ RayTracing/ray.h
@@ -1,19 +1,32 @@
-#ifndef RAYH
-#define RAYH
+#ifndef RAY_H
+#define RAY_H
 
 #include "vec3.h"
 
-class ray
-{
+class ray {
 public:
-	ray() {}
-	ray(const vec3& a, const vec3& b) { A = a; B = b; }
-	vec3 origin() const { return A; }
-	vec3 direction() const { return B; }
-	vec3 point_at_parameter(float t) { return A + t * B; }
+    ray() {}
+    ray(const point3& origin, const vec3& direction)
+        : orig(origin), dir(direction), tm(0)
+    {}
 
-	vec3 A;
-	vec3 B;
+    ray(const point3& origin, const vec3& direction, double time)
+        : orig(origin), dir(direction), tm(time)
+    {}
+
+    point3 origin() const { return orig; }
+    vec3 direction() const { return dir; }
+    double time() const { return tm; }
+
+    point3 at(double t) const
+    {
+        return orig + t * dir;
+    }
+
+public:
+    point3 orig;
+    vec3 dir;
+    double tm;
 };
 
-#endif // !RAYH
+#endif
