09c3102b1b2608e3a5e9f50c0341a1a223e50a3e
Varun Ramakrishnan <oceans.varun@gmail.com>
2022-09-19 15:13:26 -0400

    Linearly blend from white to blue

--- RayTracing/main.cpp
+++ RayTracing/main.cpp
@@ -2,7 +2,20 @@
 #include <fstream>
 #include <sstream>
 
-#include "vec3.h"
+#include "ray.h"
+
+/// <summary>
+/// Linearly blend white and blue depending on the y coordinate
+/// </summary>
+/// <param name="r">Ray</param>
+/// <returns></returns>
+vec3 lerp(const ray& r)
+{
+	vec3 unit_dir = unit_vector(r.direction()); // Make unit vector
+	float t = 0.5f * (unit_dir.y() + 1.0f); // Change range from [-1, 1] to [0, 1]
+
+	return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
+}
 
 int main()
 {
@@ -14,19 +27,25 @@ int main()
 	std::stringstream ss;
 	ss << "P3" << std::endl << nx << " " << ny << "\n255" << std::endl;
 
+	vec3 lower_left_corner(-2.0, -1.0, -1.0);
+	vec3 horizontal(4.0, 0.0, 0.0);
+	vec3 vertical(0.0, 2.0, 0.0);
+	vec3 origin(0.0, 0.0, 0.0);
+
 	for (int j = ny - 1; j >= 0; j--)
 	{
 		for (int i = 0; i < nx; i++)
 		{
-			float r = float(i) / float(nx);
-			float g = float(j) / float(ny);
-			float b = 0.2f;
-			
-			vec3 color(r, g, b);
+			float u = float(i) / float(nx);
+			float v = float(j) / float(ny);
 
-			int ir = int(255.99 * color[0]);
-			int ig = int(255.99 * color[1]);
-			int ib = int(255.99 * color[2]);
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+			
+			vec3 col = lerp(r);
+
+			int ir = int(255.99 * col[0]);
+			int ig = int(255.99 * col[1]);
+			int ib = int(255.99 * col[2]);
 
 			ss << ir << " " << ig << " " << ib << std::endl;
 		}
