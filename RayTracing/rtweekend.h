edfd65ddee6ac0ba9bce0e31fb619a9a1f7a644b
Varun Ramakrishnan <oceans.varun@gmail.com>
2022-09-20 16:19:31 -0400

    Added utilities for random and clamp

--- RayTracing/rtweekend.h
+++ RayTracing/rtweekend.h
@@ -16,9 +16,49 @@ const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 // Utility Functions
+
+/// <summary>
+/// Convert degrees to radians
+/// </summary>
+/// <param name="degrees">Angle in degrees</param>
+/// <returns>Angle in radians</returns>
 inline double degrees_to_radians(double degrees)
 {
     return degrees * pi / 180.0;
+}
+
+/// <summary>
+/// Generate a random double in [0, 1]
+/// </summary>
+/// <returns></returns>
+inline double random_double()
+{
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+/// <summary>
+/// Generate a random double in [min, max]
+/// </summary>
+/// <returns></returns>
+inline double random_double(double min, double max)
+{
+    // Returns a random real in [min,max).
+    return min + (max - min) * random_double();
+}
+
+/// <summary>
+/// Clamp a double between a minimum and a maximum
+/// </summary>
+/// <param name="x">Value to clamp</param>
+/// <param name="min">Minimum value of clamp range</param>
+/// <param name="max">Maximum value of clamp range</param>
+/// <returns></returns>
+inline double clamp(double x, double min, double max)
+{
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
 }
 
 // Common Headers
