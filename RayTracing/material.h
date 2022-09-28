b50f75dbf1767ba8bed6b1d158e55984be656dea
Varun Ramakrishnan <oceans.varun@gmail.com>
2022-09-28 17:05:52 -0400

    Added fuzziness for metal materials

--- RayTracing/material.h
+++ RayTracing/material.h
@@ -32,8 +32,8 @@ class lambertian : public material
         /// <summary>
         /// Override parent function
         /// </summary>
-        /// <param name="r_in"></param>
-        /// <param name="rec"></param>
+        /// <param name="r_in">Incident ray</param>
+        /// <param name="rec">Hit record</param>
         /// <param name="attenuation"></param>
         /// <param name="scattered"></param>
         /// <returns>True if scattered, false if miss</returns>
@@ -59,15 +59,25 @@ class metal : public material
 {
     public:
         color albedo;
+        double fuzz; // Fuzziness quotient for material
 
         metal(const color& a) : albedo(a) {}
+        metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
+        /// <summary>
+        /// Override parent function
+        /// </summary>
+        /// <param name="r_in">Incident ray</param>
+        /// <param name="rec">Hit record</param>
+        /// <param name="attenuation"></param>
+        /// <param name="scattered"></param>
+        /// <returns>True if scattered, false if miss</returns>
         virtual bool scatter(
             const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
         ) const override
         {
             vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-            scattered = ray(rec.p, reflected);
+            scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere()); // Check scattering with fuzziness
             attenuation = albedo;
             return (dot(scattered.direction(), rec.normal) > 0);
         };
