33eb1dce9f028322397a8bb4d90e1a98ca55d3a9
Varun Ramakrishnan <oceans.varun@gmail.com>
2022-09-30 15:47:49 -0400

    Added orthonormal basis vectors, and lens radius

--- RayTracing/camera.h
+++ RayTracing/camera.h
@@ -10,6 +10,8 @@ class camera
 		point3 lower_left_corner;
 		vec3 horizontal;
 		vec3 vertical;
+		vec3 u, v, w; // Orthonormal basis vectors
+		double lens_radius;
 
 		camera()
 		{
@@ -47,9 +49,71 @@ class camera
 			lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - vec3(0.0, 0.0, focal_length);
 		}
 
-		ray get_ray(double u, double v) const
+		/// <summary>
+		/// Construct an arbitrary camera with a lookfrom and lookat point,
+		/// and an up vector
+		/// </summary>
+		/// <param name="lookfrom">Point the camera is looking from</param>
+		/// <param name="lookat">Point the camera is looking at</param>
+		/// <param name="up">World up vector</param>
+		/// <param name="fov">vertical fov</param>
+		/// <param name="aspect">Aspect ratio</param>
+		camera(point3 lookfrom, point3 lookat, vec3 up, double fov, double aspect_ratio)
 		{
-			return ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+			auto theta = degrees_to_radians(fov);
+			auto h = tan(theta / 2.0f);
+
+			auto viewport_height = 2.0f * h;
+			auto viewport_width = aspect_ratio * viewport_height;
+
+			// Create orthonormal basis vectors to describe camera orientation
+			auto w = unit_vector(lookfrom - lookat);
+			auto u = unit_vector(cross(up, w));
+			auto v = cross(w, u);
+
+			origin = lookfrom;
+			horizontal = viewport_width * u;
+			vertical = viewport_height * v;
+			lower_left_corner = origin - (horizontal / 2.0f) - (vertical / 2.0f) - w;
+		}
+
+		/// <summary>
+		/// Construct an arbitrary camera with a lookfrom and lookat point,
+		/// an up vector, aperture size, and focus distance
+		/// </summary>
+		/// <param name="lookfrom">Point the camera is looking from</param>
+		/// <param name="lookat">Point the camera is looking at</param>
+		/// <param name="up">World up vector</param>
+		/// <param name="fov">vertical fov</param>
+		/// <param name="aspect">Aspect ratio</param>
+		/// <param name="aperture">Size of camera aperture</param>
+		/// <param name="focus_dist">Distance from camera at which everything is in focus</param>
+		camera(point3 lookfrom, point3 lookat, vec3 up, double fov, double aspect_ratio, double aperture, double focus_dist)
+		{
+			auto theta = degrees_to_radians(fov);
+			auto h = tan(theta / 2.0f);
+
+			auto viewport_height = 2.0f * h;
+			auto viewport_width = aspect_ratio * viewport_height;
+
+			w = unit_vector(lookfrom - lookat);
+			u = unit_vector(cross(up, w));
+			v = cross(w, u);
+
+			origin = lookfrom;
+			horizontal = focus_dist * viewport_width * u;
+			vertical = focus_dist * viewport_height * v;
+			lower_left_corner = origin - (horizontal / 2.0) - (vertical / 2.0) - (focus_dist * w);
+
+			lens_radius = aperture / 2.0;
+		}
+
+		ray get_ray(double s, double t) const
+		{
+			vec3 rd = lens_radius * random_in_unit_disk();
+			vec3 offset = (u * rd.x()) + (v * rd.y());
+
+			return ray(origin + offset, lower_left_corner + (s * horizontal) + (t * vertical) - origin - offset);
 		}
 };
 
